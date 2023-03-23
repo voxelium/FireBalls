a081c0ded5f55591da84dc918a02c6103df4be22
VOXELIUM <52653488+voxelium@users.noreply.github.com>
2023-03-23 14:09:07 +0400

    Bullet Moving

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private float _speed;
+
+    private Vector3 _moveDirection;
+
     // Start is called before the first frame update
     void Start()
     {
+        _moveDirection = Vector3.forward;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(_moveDirection * _speed * Time.deltaTime);
     }
 }
